30cbebe0ad3c286c4f3e94c82fa42a4fbfe624e3
dmerkurjev <36778179+dmerkurjev@users.noreply.github.com>
2025-09-02 15:54:18 -0700

    Update Code_to_Answers.sh

--- workflow/Code_to_Answers.sh
+++ workflow/Code_to_Answers.sh
@@ -4,23 +4,23 @@ Answers to questions path, please run full script succesfully first to run those
 # ---- Answers for q1..q5 ----
 # Requires objects 'counts', 'dds', and 'res' created above
 
-# q1: lanes concatenated for colon rep1
+# q1: How many sequencing lanes were concatenated to form sample A375rep1?
 # If you later generate a manifest upstream, read it here instead of hardcoding
 sample_lanes <- c(colon1 = 1L, colon2 = 1L, normal1 = 1L, normal2 = 1L, metastatic1 = 1L, metastatic2 =1L)
 ans_q1 <- unname(sample_lanes["colon1"])
 
-# q2: library size for normal 1?
+# q2: What sample has lower library size (total read counts):SHSY5Yrep1 or HEK293rep1?
 libsize <- colSums(counts)
 ans_q2 <- unname(libsize["sm"])
 
-# q3: genes with nonzero counts in senescent +dox
+# q3: How many genes have counts>20 in sample HEK293rep1 or counts>20 in sample HEK293rep2?
 ans_q3 <- sum(counts[, "ym"] > 0)
 
-# q4: genes upregulated ≥2 fold (log2FC ≥ 1) with FDR < 0.01
+# q4: How many genes are upregulated \uc0\u8805 2-fold (log2FC \u8805  1) in SHSY5Y vs. HEK293 with FDR < 0.01?
 res_df <- as.data.frame(res)
 ans_q4 <- sum(res_df$log2FoldChange >= 1 & res_df$padj < 0.01, na.rm = TRUE)
 
-# q5: gene ranked 2nd by log2 fold change among most upregulated
+# q5: Which gene is ranked 3th by log2 fold change (most upregulated) in A375 vs. HEK293?
 res_df$SYMBOL <- rownames(res_df)
 up_rank <- res_df %>%
   dplyr::filter(!is.na(log2FoldChange) & log2FoldChange > 0) %>%
